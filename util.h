df57106eac583794eea4001883123a830250391b
Lars Wrenger <wrenger@sra.uni-hannover.de>
2022-12-30 11:32:22 +0100

    :sparkles: Completion base barrier

--- util.h
+++ util.h
@@ -2,19 +2,22 @@
 #define __NVALLOC_UTIL__
 
 #include <linux/atomic.h>
-#include <linux/swait.h>
-#include <linux/sched.h>
+#include <linux/completion.h>
+#include <linux/mmzone.h>
 
 /// PThread-like synchronization barrier
 struct c_barrier {
-	/// Waitqueue
-	struct swait_queue_head wait;
-	/// Number of threads
-	uint max;
 	/// Number of still running threads (wait decrements this)
 	atomic_t counter;
-	/// Generation, incremented when all threads are woken up
-	atomic_t generation;
+	/// Number of managed threads
+	atomic_t threads;
+	/// Cacheline padding -> no false sharing
+	ZONE_PADDING(_pad);
+
+	/// Completions (one active, one for reinit)
+	struct completion comp[2];
+	/// The currently active completion
+	uint active;
 	/// Name of this barrier
 	const char *name;
 };
@@ -22,49 +25,41 @@ struct c_barrier {
 static inline void c_barrier_init(struct c_barrier *self, uint n,
 				  const char *name)
 {
-	init_swait_queue_head(&self->wait);
-	self->max = n;
+	init_completion(&self->comp[0]);
+	init_completion(&self->comp[1]);
 	atomic_set(&self->counter, n);
-	atomic_set(&self->generation, 0);
+	atomic_set(&self->threads, n);
+	self->active = 0;
 	self->name = name;
 }
 
 /// No threads are allowed to wait on this barrier for reinit!
 static inline void c_barrier_reinit(struct c_barrier *self, uint n)
 {
-	int ret;
-	preempt_disable();
+	uint last = self->active;
+	atomic_set(&self->threads, n);
+	atomic_set(&self->counter, n);
 
-	BUG_ON(swait_active(&self->wait));
+	// Wakeup all currently sleeping threads
+	self->active = (self->active + 1) % 2;
+	reinit_completion(&self->comp[self->active]);
 
-	ret = atomic_cmpxchg(&self->counter, self->max, n);
-	BUG_ON(ret != self->max); // no waiting threads!
-	self->max = n;
-
-	preempt_enable();
+	complete_all(&self->comp[last]);
 }
 
 static inline void c_barrier_sync(struct c_barrier *self)
 {
-	int counter;
-	preempt_disable();
-
-	counter = atomic_fetch_dec(&self->counter);
+	int counter = atomic_fetch_dec(&self->counter);
 	BUG_ON(counter <= 0);
 	if (counter == 1) {
-		atomic_set(&self->counter, self->max);
-		atomic_inc(&self->generation);
+		uint last = self->active;
+		self->active = (self->active + 1) % 2;
+		atomic_set(&self->counter, atomic_read(&self->threads));
+		reinit_completion(&self->comp[self->active]);
 
-		preempt_enable();
-		swake_up_all(&self->wait);
+		complete_all(&self->comp[last]);
 	} else {
-		int generation = atomic_read(&self->generation);
-
-		preempt_enable();
-		// Wait for wakeup
-		swait_event_exclusive(self->wait,
-				      atomic_read(&self->generation) !=
-					      generation);
+		wait_for_completion(&self->comp[self->active]);
 	}
 }
 
