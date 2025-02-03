e60d25bf3685e3465ca39d71456f6edd34a07dbe
Lars Wrenger <wrenger@sra.uni-hannover.de>
2025-02-03 16:46:55 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 make clean
 mkdir -p build-llfree-vm
-make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-llfree-vm LLVM=-14 -j50 && cp alloc.ko build-llfree-vm/alloc.keep
+make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-llfree-vm LLVM=-16 -j`nproc` && cp alloc.ko build-llfree-vm/alloc.keep
 make clean
 mkdir -p build-buddy-vm
-make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-buddy-vm LLVM=-14 -j50 && cp alloc.ko build-buddy-vm/alloc.keep
+make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-buddy-vm LLVM=-16 -j`nproc` && cp alloc.ko build-buddy-vm/alloc.keep
 make clean
 mkdir -p build-buddy-huge
-make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-buddy-huge LLVM=-14 -j50 && cp alloc.ko build-buddy-huge/alloc.keep
+make LINUX_BUILD_DIR=../llfree-linux-ballooning/build-buddy-huge LLVM=-16 -j`nproc` && cp alloc.ko build-buddy-huge/alloc.keep
 
 mv build-llfree-vm/alloc.keep build-llfree-vm/alloc.ko
 mv build-buddy-vm/alloc.keep build-buddy-vm/alloc.ko
