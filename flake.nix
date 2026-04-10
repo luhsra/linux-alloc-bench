c4c9f3c19dbbf2a7747692d3263d37e0a186da87
Lars Wrenger <wrenger@sra.uni-hannover.de>
2026-04-10 16:35:56 +0200

    :sparkles: Add nix devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "LLZero Linux Environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            buildInputs = with pkgs; [
+              lld
+              llvm
+              ncurses
+            ];
+            inputsFrom = [ pkgs.linux ];
+            hardeningDisable = [ "all" ];
+            env = { LLVM = 1; };
+          };
+        });
+    };
+}
